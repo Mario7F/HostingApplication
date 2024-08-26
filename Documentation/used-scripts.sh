3b59cc2dd215d163d035ca712ba30daf46517fbf
Mario Fletcher <mariojfletcher@gmail.com>
2024-08-26 08:17:49 -0700

    Update used-scripts.sh

--- Documentation/used-scripts.sh
+++ Documentation/used-scripts.sh
@@ -1,1 +1,18 @@
+#!/bin/bash 
 
+cd linuxproject
+mkdir helpd
+cd helpd
+vagrant init jasonc/centos8
+vi Vagrantfile
+vagrant up
+sudo dnf install -y httpd
+sudo dnf install -y php php-mysqlnd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo dnf install -y mariadb-server
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+sudo mysql_secure_installation
+mysqladmin -u root -p create helpd
+exit
